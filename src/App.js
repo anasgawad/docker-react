e762a801ffd6da312d393caa3b3af26768daa80e
Anas <anasgawad@gmail.com>
2022-11-07 18:52:00 -0700

    Change the message of App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there!
+          Hello from AWS EBS!
         </p>
         <a
           className="App-link"
